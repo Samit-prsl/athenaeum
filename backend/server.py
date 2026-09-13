e60830c6764abb7ee681d228f9f29571a1906a16
Samit Bhattacharjee <sam_prsl@Samits-MacBook-Air.local>
2026-09-14 01:25:16 +0530

    remove /

--- backend/server.py
+++ backend/server.py
@@ -54,7 +54,7 @@ app = FastAPI(title="Athenaeum", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", 'https://athenaeum-study.vercel.app/'],
+    allow_origins=["http://localhost:3000", 'https://athenaeum-study.vercel.app'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
