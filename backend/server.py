61a6770420d14ffcf6171d104fdb1a49fe690a8b
Samit Bhattacharjee <sam_prsl@Samits-MacBook-Air.local>
2026-09-14 00:46:58 +0530

    enabled CORS

--- backend/server.py
+++ backend/server.py
@@ -54,7 +54,7 @@ app = FastAPI(title="Athenaeum", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
